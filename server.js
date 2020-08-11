500fae3af4525ef0c06e826538df968ec57508ac
Mayank_MP5 <mayank5pande@gmail.com>
2020-08-11 13:58:00 +0530

    HTML and all static files in Client Folder

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  console.log("Get request to Homepage");
-  res.send("Hiii sent by server...");
-});
+app.use(express.static(`${__dirname}/client`));
+
+// app.get("/", (req, res) => {
+//   console.log("Get request to Homepage");
+//   res.send("Hiii sent by server...");
+// });
 
 //Realtime message sending socket part
 io.on("connection", function (socket) {
