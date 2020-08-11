9246d456e3617cc8372c300fbd0fd0d6fed54d54
Mayank_MP5 <mayank5pande@gmail.com>
2020-08-11 13:48:55 +0530

    Socket IO drawing data URI sending code

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get("/", (req, res) => {
   res.send("Hiii sent by server...");
 });
 
+//Realtime message sending socket part
+io.on("connection", function (socket) {
+  //On getting drawing from request emit it to all users
+  socket.on("drawing", function (dwg) {
+    io.emit("drawing", dwg);
+  });
+});
+
 http.listen(PORT, function () {
   console.log(`Server Started on PORT: ${PORT}`);
 });
